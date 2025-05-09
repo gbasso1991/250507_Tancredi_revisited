146ea3a08b748a41d414dbbebdef4cdb40278d71
Giuliano <giulianoandresbasso@gmail.com>
2025-05-09 15:38:48 -0300

    Ciclos y SAR listos para comparar

--- comparativa_tancredi.py
+++ comparativa_tancredi.py
@@ -1,4 +1,5 @@
-#%% Comparador ciclos y resultados de NE@citrato (NE250331C)
+#Comparador ciclos y resultados de NE@citrato (NE250331C)
+#%% Librerias
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -145,13 +146,10 @@ def lector_ciclos(filepath):
     M_Am  = pd.Series(data['Magnetizacion_(A/m)']).to_numpy(dtype=float)#A/m
 
     return t,H_Vs,M_Vs,H_kAm,M_Am,metadata
-#%%
+#%% Ploteo todos los ciclos 
 plot_ciclos_promedio('ex_citrato')
-
 plot_ciclos_promedio('ex_hierro')
-
 plot_ciclos_promedio('in_situ')
-#%%
 #%% Comparo a mismos idc/campo  
 idcs = [150,135,120,105,90,75,60,45,30]
 for idc in idcs:
@@ -162,28 +160,122 @@ for idc in idcs:
     _,_,_,H_insitu,M_insitu,meta_insitu = lector_ciclos(ciclos[2])
     H_max = (idc/10*float(meta_citrato['pendiente_HvsI '])+float(meta_citrato['ordenada_HvsI ']))/1000
     frec = meta_citrato['frecuencia']/1000
-    titulo=f'{H_max:.1f} kA/m - {frec:.0f} kHz'
+    titulo=f'{H_max:.1f} kA/m - {frec:.1f} kHz'
 
 
     fig, ax = plt.subplots(nrows=1,figsize=(6,5),constrained_layout=True)
-    ax.plot(H_citrato/1000,M_citrato,label='ex citrato')
-    ax.plot(H_hierro/1000,M_hierro,label='ex hierro')
-    ax.plot(H_insitu/1000,M_insitu,label='ex in situ')
-
-        
-    # for c in ciclos_PEM:
-    #     _,_,_,H_kAm,M_Am,_ = lector_ciclos(c)
-    #     ax.plot(H_kAm,M_Am,c='tab:red',label='PEM')
-
+    ax.plot(H_insitu/1000,M_insitu,c='tab:red',label='in situ')
+    ax.plot(H_citrato/1000,M_citrato,c='tab:green',label='ex citrato')
+    ax.plot(H_hierro/1000,M_hierro,c='tab:blue',label='ex hierro')
     ax.grid()
     ax.set_xlabel('H (kA/m)')
     ax.set_ylabel('M (A/m)')
     ax.set_title(titulo,fontsize=12)
-    ax.legend(ncol=1)
+    ax.legend(title='Ferrogel',ncol=1)
     # ax.set_xlim(0,60e3)
     # ax.set_ylim(0,)
-    plt.savefig('comparativa_HM_tancredi'+str(idc)+'.png',dpi=400)
+    plt.savefig('comparativa_HM_tancredi_'+str(idc)+'.png',dpi=400)
     plt.show()
 
+#%% ploteo los mismos que el paper 
+idcs_paper = [30,105,150]
+campos = []
+fig, ax = plt.subplots(nrows=1,ncols=3,figsize=(12,4),constrained_layout=True,sharex=True,sharey=True)
 
-#%%
+for i,idc in enumerate(idcs_paper):
+    ciclos_paper = glob(os.path.join('**/*2025*','*'+str(idc)+'dA'+'*ciclo_promedio*'),recursive=True)
+
+    _,_,_,H_citrato,M_citrato,meta_citrato = lector_ciclos(ciclos_paper[0])
+    _,_,_,H_hierro,M_hierro,meta_hierro = lector_ciclos(ciclos_paper[1])
+    _,_,_,H_insitu,M_insitu,meta_insitu = lector_ciclos(ciclos_paper[2])
+    H_max = (idc/10*float(meta_citrato['pendiente_HvsI '])+float(meta_citrato['ordenada_HvsI ']))/1000
+    campos.append(round(H_max,0))
+    frec = meta_citrato['frecuencia']/1000
+    titulo=f'{H_max:.1f} kA/m - {frec:.1f} kHz'
+
+    ax[i].plot(H_insitu/1000,M_insitu,c='tab:red',label='in situ')
+    ax[i].plot(H_citrato/1000,M_citrato,c='tab:green',label='ex citrato')
+    ax[i].plot(H_hierro/1000,M_hierro,c='tab:blue',label='ex hierro')
+
+    
+# for c in ciclos_PEM:
+#     _,_,_,H_kAm,M_Am,_ = lector_ciclos(c)
+#     ax.plot(H_kAm,M_Am,c='tab:red',label='PEM')
+    ax[i].grid()
+    ax[i].set_xlabel('H (kA/m)')
+    ax[i].set_title(titulo,fontsize=12)
+    ax[i].legend(title='Ferrogel',ncol=1,loc='upper left')
+    # ax.set_xlim(0,60e3)
+    # ax.set_ylim(0,)
+ax[0].set_ylabel('M (A/m)')
+plt.savefig('comparativa_HM_tancredi_030_105_150.png',dpi=400)
+plt.show()
+
+#%% SAR
+res_150=glob(os.path.join('**/*2025*','*150*'+'*resultados*'),recursive=True)
+res_105=glob(os.path.join('**/*2025*','*105*'+'*resultados*'),recursive=True)
+res_030=glob(os.path.join('**/*2025*','*030*'+'*resultados*'),recursive=True)
+res_150.sort()
+res_105.sort()
+res_030.sort()
+
+meta_150_exc, _,_,_,_,_,_,_,_,_,_,_,SAR_150_exc,_,_=lector_resultados(res_150[0])
+meta_150_exh, _,_,_,_,_,_,_,_,_,_,_,SAR_150_exh,_,_=lector_resultados(res_150[1])
+meta_150_ins, _,_,_,_,_,_,_,_,_,_,_,SAR_150_ins,_,_=lector_resultados(res_150[2])
+
+meta_105_exc, _,_,_,_,_,_,_,_,_,_,_,SAR_105_exc,_,_=lector_resultados(res_105[0])
+meta_105_exh, _,_,_,_,_,_,_,_,_,_,_,SAR_105_exh,_,_=lector_resultados(res_105[1])
+meta_105_ins, _,_,_,_,_,_,_,_,_,_,_,SAR_105_ins,_,_=lector_resultados(res_105[2])
+
+meta_030_exc, _,_,_,_,_,_,_,_,_,_,_,SAR_030_exc,_,_=lector_resultados(res_030[0])
+meta_030_exh, _,_,_,_,_,_,_,_,_,_,_,SAR_030_exh,_,_=lector_resultados(res_030[1])
+meta_030_ins, _,_,_,_,_,_,_,_,_,_,_,SAR_030_ins,_,_=lector_resultados(res_030[2])
+
+sar_150_exc=ufloat(np.mean(SAR_150_exc),np.std(SAR_150_exc))
+sar_150_exh=ufloat(np.mean(SAR_150_exh),np.std(SAR_150_exh))
+sar_150_ins=ufloat(np.mean(SAR_150_ins),np.std(SAR_150_ins))
+
+sar_105_exc=ufloat(np.mean(SAR_105_exc),np.std(SAR_105_exc))
+sar_105_exh=ufloat(np.mean(SAR_105_exh),np.std(SAR_105_exh))
+sar_105_ins=ufloat(np.mean(SAR_105_ins),np.std(SAR_105_ins))
+
+sar_030_exc=ufloat(np.mean(SAR_030_exc),np.std(SAR_030_exc))
+sar_030_exh=ufloat(np.mean(SAR_030_exh),np.std(SAR_030_exh))
+sar_030_ins=ufloat(np.mean(SAR_030_ins),np.std(SAR_030_ins))
+
+# %%
+
+# Configuración del gráfico
+x = [1/3,2/3,1]  # Posiciones de las barras
+width = 0.05  # Ancho de las barras
+delta= 0.051
+fig2, ax = plt.subplots(nrows=1,figsize=(6,5),constrained_layout=True,sharex=True)
+
+bar11 = ax.bar(x[0]-delta,sar_030_ins.n, width, yerr=sar_030_ins.s, capsize=7, color='tab:red', label='In situ')
+bar13 = ax.bar(x[0],sar_030_exc.n, width, yerr=sar_030_exc.s, capsize=7, color='tab:green', label='Ex citrato')
+bar13 = ax.bar(x[0]+delta,sar_030_exh.n, width, yerr=sar_030_exh.s, capsize=7, color='tab:blue', label='Ex hierro')
+
+bar21 = ax.bar(x[1]-delta,sar_105_ins.n, width, yerr=sar_105_ins.s, capsize=7, color='tab:red')
+bar23 = ax.bar(x[1],sar_105_exc.n, width, yerr=sar_105_exc.s, capsize=7, color='tab:green')
+bar23 = ax.bar(x[1]+delta,sar_105_exh.n, width, yerr=sar_105_exh.s, capsize=7, color='tab:blue')
+
+bar31 = ax.bar(x[2]-delta,sar_150_ins.n, width, yerr=sar_150_ins.s, capsize=7, color='tab:red')
+bar33 = ax.bar(x[2],sar_150_exc.n, width, yerr=sar_150_exc.s, capsize=7, color='tab:green')
+bar33 = ax.bar(x[2]+delta,sar_150_exh.n, width, yerr=sar_150_exh.s, capsize=7, color='tab:blue')
+
+ax.set_xticks(x)
+ax.grid(axis='y', linestyle='--', alpha=0.7)
+ax.set_xticklabels(campos)
+ax.set_xlabel('H (kA/m)')
+ax.legend(ncol=1,title='Ferrogel')
+ax.set_ylabel('SAR (W/g)')
+
+
+# ax1.set_ylabel('Hc (kA/m)', fontsize=12)
+# ax1.set_title('Coercitivo vs Frecuencia',loc='left', fontsize=13)
+# ax2.set_title('Remanencia vs Frecuencia',loc='left', fontsize=13)
+# ax2.set_xlabel('f (kHz)', fontsize=12)
+plt.suptitle('SAR - 268 kHz')
+plt.savefig('comparativa_HM_tancredi_030_105_150.png',dpi=400)
+plt.show()
+# %%
